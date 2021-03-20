95246d8b2e317e6454ca3bb51ee7ce107357f029
Gustavo Belo <gustavobelo28@gmail.com>
2021-03-20 20:39:07 -0300

    Initial Commit

---
+++ Good Memory/Good_MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_MemoryApp.swift
+//  Good Memory
+//
+//  Created by Gustavo Belo on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Good_MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
